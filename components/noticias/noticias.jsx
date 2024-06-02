2287c9bb524148cb97ad8076bc49afc2bbb4f179
alex-ohdz <alejandrooramash@gmail.com>
2024-06-01 22:06:32 -0400

    oka

--- components/noticias/noticias.jsx
+++ components/noticias/noticias.jsx
@@ -15,30 +15,16 @@ const notices = [
     title: "Nombre del Trabajador 3",
     text: "Cargo o Rol",
     imageUrl: "https://rickandmortyapi.com/api/character/avatar/291.jpeg",
-  },
-  {
-    title: "Nombre del Trabajador 1",
-    text: "Cargo o Rol",
-    imageUrl: "https://rickandmortyapi.com/api/character/avatar/290.jpeg",
-  },
-  {
-    title: "Nombre del Trabajador 2",
-    text: "Cargo o Rol",
-    imageUrl: "https://rickandmortyapi.com/api/character/avatar/450.jpeg",
-  },
-  {
-    title: "Nombre del Trabajador 3",
-    text: "Cargo o Rol",
-    imageUrl: "https://rickandmortyapi.com/api/character/avatar/291.jpeg",
-  },
+  }
 ];
 
 const Noticias = () => {
   return (
-    <div className="container mx-auto my-10 px-4">
-      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-7 place-items-center">
+    <div className="mx-auto px-4 bg-slate-100 pb-10">
+      <h1 className="text-center font-serif text-3xl py-10 text-amber-800">Actividades Recientes</h1>
+      <div className="cards">
         {notices.map((n, index) => (
-          <Cards />
+          <Cards key={index} title={n.title} text={n.text} imageUrl={n.imageUrl} />
         ))}
       </div>
     </div>
