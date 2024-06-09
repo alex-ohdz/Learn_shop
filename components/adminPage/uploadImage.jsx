db445bf889c468d60237b8d79d24de9114a1fb71
alex-ohdz <alejandrooramash@gmail.com>
2024-06-09 12:49:12 -0400

    homecarouselOKAOKA

--- components/adminPage/uploadImage.jsx
+++ components/adminPage/uploadImage.jsx
@@ -1,10 +1,12 @@
 "use client";
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ImageInput from "./imageInput";
 import LinearProgress from "@mui/material/LinearProgress";
 import Box from "@mui/material/Box";
 import { styled } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const CustomLinearProgress = styled(LinearProgress)({
   height: 10, // Ajusta la altura de la barra de progreso
@@ -18,8 +20,27 @@ const CustomLinearProgress = styled(LinearProgress)({
 
 function UploadImages() {
   const [selectedFiles, setSelectedFiles] = useState([]);
+  const [uploadedImages, setUploadedImages] = useState([]);
   const [uploadProgress, setUploadProgress] = useState(0);
   const [uploading, setUploading] = useState(false);
+
+  useEffect(() => {
+    // Fetch images from the database on component mount
+    const fetchImages = async () => {
+      try {
+        const response = await axios.get("/api/get-images");
+        if (response.status === 200) {
+          setUploadedImages(response.data);
+        } else {
+          console.error("Error fetching images");
+        }
+      } catch (error) {
+        console.error("Error fetching images", error);
+      }
+    };
+
+    fetchImages();
+  }, []);
 
   const handleFilesSelected = (files) => {
     if (files.length + selectedFiles.length > 10) {
@@ -27,6 +48,23 @@ function UploadImages() {
       return;
     }
     setSelectedFiles((prevFiles) => [...prevFiles, ...Array.from(files)]);
+  };
+
+  const handleDeleteSelected = (index) => {
+    setSelectedFiles((prevFiles) => prevFiles.filter((_, i) => i !== index));
+  };
+
+  const handleDeleteUploaded = async (id) => {
+    try {
+      const response = await axios.delete("/api/delete-image", { data: { id } });
+      if (response.status === 200) {
+        setUploadedImages((prevImages) => prevImages.filter((image) => image.id !== id));
+      } else {
+        console.error("Error deleting image");
+      }
+    } catch (error) {
+      console.error("Error deleting image", error);
+    }
   };
 
   const handleUpload = async () => {
@@ -52,6 +90,20 @@ function UploadImages() {
         const result = response.data;
         console.log("Imágenes subidas:", result);
         setSelectedFiles([]); // Limpiar selección después de la subida
+        // Refetch images after upload
+        const fetchImages = async () => {
+          try {
+            const response = await axios.get("/api/get-images");
+            if (response.status === 200) {
+              setUploadedImages(response.data);
+            } else {
+              console.error("Error fetching images");
+            }
+          } catch (error) {
+            console.error("Error fetching images", error);
+          }
+        };
+        fetchImages();
       } else {
         console.error("Error subiendo las imágenes");
       }
@@ -67,12 +119,21 @@ function UploadImages() {
       <ImageInput onFilesSelected={handleFilesSelected} />
       <div className="mt-4 mx-10 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
         {selectedFiles.map((file, index) => (
-          <div key={index} className="relative">
-            <img
-              src={URL.createObjectURL(file)}
-              alt={`preview-${index}`}
-              className="w-full h-full object-cover rounded"
-            />
+          <div key={index} className="relative h-50">
+            <div>
+              <img
+                src={URL.createObjectURL(file)}
+                alt={`preview-${index}`}
+                className="w-full h-full object-cover rounded"
+              />
+              <IconButton
+                aria-label="delete"
+                className="absolute top-0 right-0"
+                onClick={() => handleDeleteSelected(index)}
+              >
+                <DeleteIcon style={{ color: "red" }} />
+              </IconButton>
+            </div>
           </div>
         ))}
       </div>
@@ -104,6 +165,26 @@ function UploadImages() {
       >
         Subir imágenes
       </button>
+      <div className="mt-4 mx-10 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+        {uploadedImages.map((image, index) => (
+          <div key={index} className="relative mt-10">
+            <div>
+              <img
+                src={`data:image/jpeg;base64,${image.data}`}
+                alt={`uploaded-${index}`}
+                className="w-full h-full object-cover rounded"
+              />
+              <IconButton
+                aria-label="delete"
+                className="absolute top-0 right-0"
+                onClick={() => handleDeleteUploaded(image.id)}
+              >
+                <DeleteIcon style={{ color: "red" }} />
+              </IconButton>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
